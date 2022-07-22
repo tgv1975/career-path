5601c24b4fc7bfcea3ce118c1d9c30351e446c17
Tudor Gavan <63089190+pp-tudorg@users.noreply.github.com>
2022-07-22 18:16:23 +0300

    Include build in git

---
+++ build/static/js/455.f7167e62.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcareer_path=self.webpackChunkcareer_path||[]).push([[455],{5455:function(e,a,c){c.r(a),a.default={}}}]);
+//# sourceMappingURL=455.f7167e62.chunk.js.map
---
+++ build/static/js/521.91581b9a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcareer_path=self.webpackChunkcareer_path||[]).push([[521],{2521:function(e,a,c){c.r(a),a.default={}}}]);
+//# sourceMappingURL=521.91581b9a.chunk.js.map
